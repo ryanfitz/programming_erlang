584090a706732760e126ef01267233e81beae592
Ryan Fitzgerald <ryan@codebrewstudios.com>
2013-11-02 21:12:13 -0400

    chap 4 ex 6

--- math_functions.erl
+++ math_functions.erl
@@ -1,9 +1,12 @@
 
 -module(math_functions).
--export([even/1, odd/1]).
+-export([even/1, odd/1, filter/2]).
 
 even(X) when X rem 2 =:= 1 -> false;
 even(X) when is_integer(X) -> true.
 
 odd(X) when is_integer(X) ->
   not even(X).
+
+filter(F,L) ->
+  [X || X <- L, F(X) =:= true].
